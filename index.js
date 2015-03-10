dfb460daed6d259403b36042d83c8b78913299de
Raymond Asuncion <mon@englishcentral.com>
2015-03-10 22:18:07 +0800

    Modified how classname is generated to be usable in sonar

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
   var log = logger.create('reporter.junit');
   var reporterConfig = config.junitReporter || {};
   var pkgName = reporterConfig.suite || '';
-  var outputFile = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputFile
-      || 'test-results.xml'));
 
-  var xml;
+  if (!reporterConfig.outputDir) {
+    throw new Error('You must set an output directory for JUnitReporter via the outputDir config property');
+  }
+
+  var outputDir = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputDir)) + path.sep;
   var suites;
   var pendingFileWritings = 0;
   var fileWritingFinished = function() {};
@@ -25,49 +27,22 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
 
   var initliazeXmlForBrowser = function(browser) {
     var timestamp = (new Date()).toISOString().substr(0, 19);
-    var suite = suites[browser.id] = xml.ele('testsuite', {
-      name: browser.name, 'package': pkgName, timestamp: timestamp, id: 0, hostname: os.hostname()
-    });
-    suite.ele('properties').ele('property', {name: 'browser.fullName', value: browser.fullName});
+    var suite = suites[browser.id] = builder.create('testsuite');
+    suite.att('name', browser.name)
+         .att('package', pkgName)
+         .att('timestamp', timestamp)
+         .att('id', 0)
+         .att('hostname', os.hostname());
+    suite.ele('properties')
+         .ele('property', {name: 'browser.fullName', value: browser.fullName});
   };
 
-  this.onRunStart = function(browsers) {
-    suites = Object.create(null);
-    xml = builder.create('testsuites');
-
-    // TODO(vojta): remove once we don't care about Karma 0.10
-    browsers.forEach(initliazeXmlForBrowser);
-  };
-
-  this.onBrowserStart = function(browser) {
-    initliazeXmlForBrowser(browser);
-  };
-
-  this.onBrowserComplete = function(browser) {
-    var suite = suites[browser.id];
-
-    if (!suite) {
-      // This browser did not signal `onBrowserStart`. That happens
-      // if the browser timed out during the start phase.
-      return;
-    }
-
-    var result = browser.lastResult;
-
-    suite.att('tests', result.total);
-    suite.att('errors', result.disconnected || result.error ? 1 : 0);
-    suite.att('failures', result.failed);
-    suite.att('time', (result.netTime || 0) / 1000);
-
-    suite.ele('system-out').dat(allMessages.join() + '\n');
-    suite.ele('system-err');
-  };
-
-  this.onRunComplete = function() {
-    var xmlToOutput = xml;
+  var writeXmlForBrowser = function(browser) {
+    var outputFile = outputDir + 'TESTS-' + browser.name.replace(/ /g, '_') + '.xml';
+    var xmlToOutput = suites[browser.id];
 
     pendingFileWritings++;
-    helper.mkdirIfNotExists(path.dirname(outputFile), function() {
+    helper.mkdirIfNotExists(outputDir, function() {
       fs.writeFile(outputFile, xmlToOutput.end({pretty: true}), function(err) {
         if (err) {
           log.warn('Cannot write JUnit xml\n\t' + err.message);
@@ -80,15 +55,43 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
         }
       });
     });
+  };
 
-    suites = xml = null;
+  this.onRunStart = function(browsers) {
+    suites = Object.create(null);
+
+    // TODO(vojta): remove once we don't care about Karma 0.10
+    browsers.forEach(initliazeXmlForBrowser);
+  };
+
+  this.onBrowserStart = function(browser) {
+    initliazeXmlForBrowser(browser);
+  };
+
+  this.onBrowserComplete = function(browser) {
+    var suite = suites[browser.id];
+    var result = browser.lastResult;
+
+    suite.att('tests', result.total);
+    suite.att('errors', result.disconnected || result.error ? 1 : 0);
+    suite.att('failures', result.failed);
+    suite.att('time', (result.netTime || 0) / 1000);
+
+    suite.ele('system-out').dat(allMessages.join() + '\n');
+    suite.ele('system-err');
+
+    writeXmlForBrowser(browser);
+  };
+
+  this.onRunComplete = function() {
+    suites = null;
     allMessages.length = 0;
   };
 
   this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
     var spec = suites[browser.id].ele('testcase', {
       name: result.description, time: ((result.time || 0) / 1000),
-      classname: (pkgName ? pkgName + ' ' : '') + browser.name + '.' + result.suite.join(' ').replace(/\./g, '_')
+      classname: browser.name.replace(/ /g,'_').replace(/\./g,'_') + '.' + (pkgName ? pkgName + '.' : '') + result.suite[0] 
     });
 
     if (result.skipped) {
